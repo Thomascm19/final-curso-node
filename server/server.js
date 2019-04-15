f8f5ddaa8783488da51bb3a9b71100c57c837d63
Thomas <thomascaycedo19@outlook.com>
2019-04-15 17:49:30 -0500

    Se actualiza el server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use( require ('./routes/routes') )
 
 
 //Se realiza la conexion a la DB
-mongoose.connect(process.env.URLDB, {useNewUrlParser: true}, (err,res) => {
+mongoose.connect(process.env.URLDB, {useNewUrlParser: true,useCreateIndex: true}, (err,res) => {
 
 if(err) throw err;
 
