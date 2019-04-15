9bcc8c002d6b000ccd8e498c02c8d28fec3780d8
Thomas <thomascaycedo19@outlook.com>
2019-04-15 17:28:11 -0500

    Se agregan las variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,22 @@
-//Puerto
+/*===============================
+            Puerto
+=================================
+*/
 process.env.PORT = process.env.PORT || 3000;
+/*===============================
+            Entorno
+=================================
+*/
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+/*===============================
+            Base de datos
+===============================
+*/
+let urlDB;
 
-process.env.URLDB = 'mongodb://localhost:27017/school';
+if(process.env.NODE_ENV === 'DEV'){
+    urlDB = 'mongodb://localhost:27017/school';
+}else{
+    urlDB = process.env.MONGO_URI; 
+}
+process.env.URLDB = urlDB;
