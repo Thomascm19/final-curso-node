2295ee52d617878a9ef9d1b0cc3bf2da55311905
Thomas <thomascaycedo19@outlook.com>
2019-04-15 19:27:38 -0500

    Se redirecciona la pagina principal

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -14,7 +14,7 @@ hbs.registerPartials(__dirname + '../../../views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.redirect('home');
+    res.redirect('/cursosDisponibles');
 })
 app.get('/cursosDisponibles', (req, res) => {
     res.render('cursosDisponibles');
